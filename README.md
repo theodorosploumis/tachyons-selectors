82ba9fabe028f9fb6a3640ac34d7e46620923573
TheodorosPloumis <me@theodorosploumis.com>
2018-03-30 14:16:13 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,27 @@
+# Generated Tachyons CSS classes
+
+I have created this mini script so I can be able to **create html element options** (eg select, checkbox, radio) that will use the CSS classes from Tachyons as values.
+
+Works with any css file and selector type though. Is is based on the [/davidtheclark/list-selectors](https://github.com/davidtheclark/list-selectors) library so you can find the examples there.
+
+See some of the generated files:
+
+- [tachyons_classes.md]("https://github.com/theodorosploumis/tachyons-selectors/tachyons_classes.md)
+- [tachyons_classes_verbose.md]("https://github.com/theodorosploumis/tachyons-selectors/tachyons_classes_verbose.md)
+
+# Install
+```
+// CLone locally
+git clone git@github.com:theodorosploumis/tachyons-selectors.git
+cd tachyons-selectors
+
+// Install packages
+npm install
+
+// Generate files
+node index.js
+```
+
+# Licence
+
+MIT
---
+++ index.js
@@ -0,0 +1,38 @@
+var listSelectors = require('list-selectors'),
+    fs = require('fs');
+
+var version = "4.9.1",
+    file = "tachyons_classes.md",
+    file_verbose = "tachyons_classes_verbose.md";
+
+var styles = "https://unpkg.com/tachyons@"+version+"/css/tachyons.min.css";
+var styles_verbose = "https://rawgit.com/tachyons-css/tachyons-verbose/master/css/tachyons.min.css";
+
+// Get all css classes from a css file (source)
+function writeSelectors(source, options, filename) {
+  listSelectors(
+    [source],
+    { include: [options] },
+    function(List) {
+      var Selectors = [],
+          New = List[options],
+          stream = fs.createWriteStream(filename);
+
+      stream.once('open', function(fd) {
+        stream.write("## Tachyons Classes - version "+version+" \n\n");
+        stream.write("```\n");
+        for (var i = 0; i <= New.length; i++) {
+          if (New[i] !== "undefined") {
+            stream.write(New[i] + "\n");
+          }
+        }
+        stream.write("```\n");
+        stream.end();
+      });
+    }
+  );
+}
+
+// Generate files
+writeSelectors(styles, "classes", file);
+writeSelectors(styles_verbose, "classes", file_verbose);
